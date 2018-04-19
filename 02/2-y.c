8f524e96e29ff3d3d73b814f1d6a7606587494b0
leafia8 <38201021+leafia8@users.noreply.github.com>
2018-04-19 17:18:09 +0900

    update

--- 02/2-y.c
+++ 02/2-y.c
@@ -3,14 +3,14 @@
 int main(void){
   double x, xx, x250, r, K;
   int t;
-  x250=90.0;
+  x250=88.0;
   K=100.0;
 
   FILE *fp;
   fp=fopen("output,dat", "w");
-
+  fprintf(fp, "%f, %f\n", r, x250);
+  
   for(r=1.0; r<3.0; r+=0.01){
-
   x=x250;
   for(t=251; t<300; t++){
     xx=x+r*(1-x/K)*x;
