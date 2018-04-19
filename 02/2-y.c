e333e21c6d0424545163611479ef0a3f05c3ed37
leafia8 <38201021+leafia8@users.noreply.github.com>
2018-04-19 15:22:33 +0900

    update

--- 02/2-y.c
+++ 02/2-y.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-  double x, xx, x0, r, K;
+  double x, xx, x250, r, K;
   int t;
-  x0=1.0;
-  r=1.7;
+  x250=90.0;
   K=100.0;
 
   FILE *fp;
   fp=fopen("output,dat", "w");
 
-  fprintf(fp, "%d, %f\n", 0, x0);
-  x=x0;
-  for(t=1; t<300; t++){
+  for(r=1.0; r<3.0; r+=0.01){
+
+  x=x250;
+  for(t=251; t<300; t++){
     xx=x+r*(1-x/K)*x;
-    fprintf(fp, "%d, %f\n", t, xx);
+    fprintf(fp, "%f, %f\n", r, xx);
     x=xx;
   }
+}
   fclose(fp);
 
   return 0;
