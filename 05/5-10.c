f66be818c2b790a3105368810042b27c5dd2ffb7
leafia8 <38201021+leafia8@users.noreply.github.com>
2018-05-20 00:34:14 +0900

    update

--- 05/5-10.c
+++ 05/5-10.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 50
+#define N 200
 
 int main(void){
   int a[N],aa[N],i,t,r1,r2,r,k,A,S,Y,T;
-  k=25;
-  T=0;
-  Y=0;
+
   srand(time(NULL));
 
+FILE *fp;
+fp=fopen("5-10fp.dat","w");
+
+for(k=(N/10);k<N+1;k+=(N/10)){
+  T=0;
+  Y=0;
 for(S=0; S<1000; S++){
   for(i=0;i<k;i++){
     a[i]=0;
@@ -21,7 +25,7 @@ for(S=0; S<1000; S++){
   for(i=0;i<N;i++){
   }
 
-    for(t=0;t<100;t++){
+    for(t=0;t<500;t++){
       A=0;
       for(i=0;i<N;i++){
         r1=rand()%N;
@@ -43,7 +47,7 @@ for(S=0; S<1000; S++){
     }
       if(A==0){
         Y=Y+1;  //成功した回数の合計
-        T=T+t+2; //変異体になった時の合計の世代数　
+        T=T+t; //変異体になった時の合計の世代数　
         break;
       }
   }
@@ -53,6 +57,8 @@ for(S=0; S<1000; S++){
     break;
   }
 }
-printf("%d\n",T/100 );
+fprintf(fp,"%d\n",T/100);
+}
+fclose(fp);
     return 0;
 }
