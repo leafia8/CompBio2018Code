af27c19f945f637d9f2510d0838d91241701b14a
leafia8 <38201021+leafia8@users.noreply.github.com>
2018-05-28 13:15:49 +0900

    update

---
+++ 06/6-7.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(void){
+  int i;
+  double t,dt,S,SS,I,II,R,RR;
+  double b,c;
+
+  FILE *fp;
+  fp=fopen("6-7op.csv","w");
+
+  dt=0.0001;
+  b=0.002;
+  c=0.1;
+
+  t=0.0;
+  S=1000.0;
+  I=500.0;
+  R=10.0;
+
+  fprintf(fp,"%f,%f,%f,%f \n",t,S,I,R);
+
+  for(i=1;i<100000;i++){
+    t=dt*i;
+    SS=(1-b*I*dt)*S;
+    II=(1+b*S*dt-c*dt)*I;
+    RR=R+c*I*dt;
+
+    S=SS;
+    I=II;
+    R=RR;
+
+    if(i%100==0){
+      fprintf(fp,"%f,%f,%f,%f \n",t,S,I,R);
+    }
+  }
+  fclose(fp);
+  return 0;
+}
